3f84f33bbf5cc144fcdfebf1876e4e285e3f3d29
gomezfox <stephen@gomezfox.net>
2022-02-23 14:53:55 -0800

    Implements signup and user object

--- app.py
+++ app.py
@@ -6,15 +6,22 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from resources.user import User, Users
+from resources.auth import Signup, Login
 import sqlite3
 from os.path import exists
 from src.migrate_db import init_db
 from src.const import DB_NAME, DB_DIRECTORY
 from flask import g
 
+
 app = Flask(__name__)
 app.config.from_object('src.config.DevelopmentConfig')
 api = Api(app)
+
+# TODO: move the in-memory token store to the database
+# simple in-memory token store
+token_store = []
+
 
 # create and populate the database if the db file doesn't exist
 if not exists(DB_DIRECTORY + DB_NAME):
@@ -29,9 +36,10 @@ class HelloWorld(Resource):
 
 # Associate resource paths with resource classes
 api.add_resource(HelloWorld, '/')
-api.add_resource(User, '/users/<string:user_name>', '/users/create')
+api.add_resource(User, '/users/<string:user_name>')
 api.add_resource(Users, '/users')
-
+api.add_resource(Signup, '/signup')
+api.add_resource(Login, '/login')
 
 # neatly exit in case of exception
 @app.teardown_appcontext
