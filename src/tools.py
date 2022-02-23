ff1403c381848e6108fffa5c58391058a1e2717d
gomezfox <stephen@gomezfox.net>
2022-02-23 15:00:41 -0800

    Removes unneded decorators, adds imports

--- src/tools.py
+++ src/tools.py
@@ -1,16 +1,17 @@
 import json
 import re
+import jwt
+from datetime import datetime, timedelta
+from flask import current_app
 
 
 # STATIC HELPER FUNCTIONS
 
-@staticmethod
+
 def toDict(result, row):
     dict(zip([c[0] for c in result.description], row))
-    # set the arguments to be parsed out of the request (JSON)
-    #
 
-@staticmethod
+
 def valid_username(user_name):
     # returns true if the is alphanumeric (plus _)and starts with a letter
     un = str(user_name)
@@ -22,17 +23,18 @@ def valid_username(user_name):
     return True
 
 
-@staticmethod
 def valid_password(password):
     # returns true if password is at least 4 characters
     pw = str(password)
     return len(pw) >= 4
 
 
-@staticmethod
 def decode_auth_token(auth_token):
     try:
-        jwt_payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+        jwt_payload = jwt.decode(
+            auth_token,
+            current_app.config.get('SECRET_KEY')
+        )
         return jwt_payload['sub']
     except jwt.ExpiredSignatureError:
         return json.dumps({'error': 'Signature expired. Please log in again.'})
@@ -40,16 +42,16 @@ def decode_auth_token(auth_token):
         return json.dumps({'error': 'Invalid token. Please log in again.'})
 
 
-@staticmethod
 def encode_auth_token(user_name):
     try:
-        payload = {
+        jwt_payload = {
             'exp': datetime.utcnow() + timedelta(days=1),
             'iat': datetime.utcnow(),
             'sub': user_name
         }
         return jwt.encode(
-            payload, app.config.get('SECRET_KEY'),
+            jwt_payload,
+            current_app.config.get('SECRET_KEY'),
             algorithm='HS256'
         )
     except Exception as e:
